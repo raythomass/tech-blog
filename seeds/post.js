23abf2f06b8d1c8f7734f67b627cd631d9fa143c
Ray Thomas <rthomas5678@gmail.com>
2024-01-08 22:19:20 -0700

    added changes

--- seeds/post.js
+++ seeds/post.js
@@ -5,7 +5,17 @@ const postData = [
     title: 'Hello Blog',
     content: 'This is my first blog',
     writer: 1
-}
+},
+{
+    title: 'Hello Blog number 2',
+    content: 'This is my second blog',
+    writer: 1
+},
+{
+    title: 'Hello Blog 3',
+    content: 'This is my third blog',
+    writer: 1
+},
 ];
 
 const seedPosts = () => BlogPost.bulkCreate(postData);
